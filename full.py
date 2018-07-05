ebc26db2a101f89a1e322aa04986bbaa5931e74e
Alex <zander5391@gmail.com>
2018-07-05 12:43:39 -0400

    Cleaned up and added modified file name

--- full.py
+++ full.py
@@ -2,9 +2,7 @@
 Created on Jul 4, 2018
 
 TODO: Add parallelism
-TODO: Write back to spreadsheet
-TODO: Make filename start with file id so pack updates can be automated
-TODO: Move more variables to the external variable json
+TODO: Make sample var file for repo
 
 @author: Alex
 '''
@@ -16,6 +14,7 @@ import urllib.request
 from lxml import html
 import os.path
 import requests
+# Imports for Sheets API
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
@@ -49,16 +48,11 @@ if not CREDS or CREDS.invalid:
     CREDS = tools.run_flow(FLOW, STORE)
 SERVICE = build('sheets', 'v4', http=CREDS.authorize(Http()))
 
-# Look for existing data and get it if it doesn't exist
+# Call the Sheets API
 # RESULT_1 is a range of values that will contain how many mods are in the list and when this program was last run 
-try:
-    with open('data_1.json') as FILE:
-        RESULT_1 = json.load(FILE)
-except FileNotFoundError:
-    # Call the Sheets API
-    RESULT_1 = SERVICE.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_1).execute()                                  
-    with open('data_1.json', 'w') as OUTFILE:  
-        json.dump(RESULT_1, OUTFILE)
+RESULT_1 = SERVICE.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_1).execute()                                  
+with open('data_1.json', 'w') as OUTFILE:  
+    json.dump(RESULT_1, OUTFILE)
 
 # Use RESULT_1 to determine how many cells to request for RESULT_2
 NUM_MODS = RESULT_1.get('values')[0][1]
@@ -66,15 +60,10 @@ RANGE_2_BEGIN = RANGE_2_PRE[-1:]
 RANGE_2_END = int(NUM_MODS) + int(RANGE_2_BEGIN) - 1
 RANGE_2 = RANGE_2_PRE[:-1] + str(RANGE_2_END)
 
-# Look for existing data and get it if it doesn't exist
 # RESULT_2 contains: mod names, link, old file id, and a download link
-try:
-    with open('data_2.json') as FILE:
-        RESULT_2 = json.load(FILE)
-except FileNotFoundError:
-    RESULT_2 = SERVICE.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_2).execute()               
-    with open('data_2.json', 'w') as OUTFILE:  
-        json.dump(RESULT_2, OUTFILE)
+RESULT_2 = SERVICE.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_2).execute()               
+with open('data_2.json', 'w') as OUTFILE:  
+    json.dump(RESULT_2, OUTFILE)
 
 # Use the project id from RESULTS_2 to build the Curse URL and get the files page
 # then find the latest jar and add it to a list to download
@@ -93,26 +82,31 @@ for line in MODS_ONLY:
         FILENAME = TABLE.xpath('//div[@class="project-file-name-container"]/a/@data-name')[0].replace(' ', '')
         if FILENAME[-4:] != '.jar':
             FILENAME += '.jar'
+        MODIFIED_FILENAME = NEW_FILE_ID + '-' + FILENAME
     if NEW_FILE_ID > OLD_FILE_ID:
         MODS_NEEDING_UPDATES.append(MOD_NAME)
-        FILES_TO_DOWNLOAD[FILENAME] = DOWNLOAD_URL
+        FILES_TO_DOWNLOAD[MODIFIED_FILENAME] = DOWNLOAD_URL
         line[3] = NEW_FILE_ID
     line[4] = DOWNLOAD_URL
 
-# List mods we need to update
-print('Update required for the following ', len(MODS_NEEDING_UPDATES), ' mods:')
-for MOD in MODS_NEEDING_UPDATES:
-    print(MOD)
-print()
-
-# Write the updated info back to the sheet
-BODY = {'values': MODS_ONLY}
-RESULT_3 = SERVICE.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=RANGE_2, valueInputOption='USER_ENTERED', body=BODY).execute()
-
-# Download the updated mods
-for ENTRY in FILES_TO_DOWNLOAD:
-    FILE_PATH = LOCAL_PATH + ENTRY
-    if os.path.isfile(FILE_PATH):
-        print('Already exists: ', ENTRY)
-    else:
-            urllib.request.urlretrieve(FILES_TO_DOWNLOAD[ENTRY], FILE_PATH)
+# See if any mods need updating
+if len(MODS_NEEDING_UPDATES) > 0:
+    # List mods we need to update
+    print('Update required for the following', len(MODS_NEEDING_UPDATES), 'mods:')
+    for MOD in MODS_NEEDING_UPDATES:
+        print(MOD)
+    print()
+    
+    # Write the updated info back to the sheet
+    BODY = {'values': MODS_ONLY}
+    RESULT_3 = SERVICE.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=RANGE_2, valueInputOption='USER_ENTERED', body=BODY).execute()
+    
+    # Download the updated mods
+    for ENTRY in FILES_TO_DOWNLOAD:
+        FILE_PATH = LOCAL_PATH + ENTRY
+        if os.path.isfile(FILE_PATH):
+            print('Already exists: ', ENTRY)
+        else:
+                urllib.request.urlretrieve(FILES_TO_DOWNLOAD[ENTRY], FILE_PATH)
+else:
+    print('Looks like all the mods are currently up to date')
